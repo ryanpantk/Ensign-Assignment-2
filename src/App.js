a7d227f3c6c12695092b94a23435406b33c49eb2
Ryan <ryanpantk@gmail.com>
2024-03-16 20:25:13 +0800

    chore: initial react commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Home from './Pages/Home';
+import Cart from './Pages/Cart';
+
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
