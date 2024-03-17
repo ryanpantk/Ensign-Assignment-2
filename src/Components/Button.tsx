751a33dbd84d0028a4966651068f584f30788a79
Ryan <ryanpantk@gmail.com>
2024-03-17 13:58:06 +0800

    feat: add button types

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -15,3 +15,25 @@ export function PrimaryButton({ text, onClick }: ButtonProps) {
         </button>
     );
 }
+
+export function SquareButton({ text, onClick }: ButtonProps) {
+    return (
+        <button
+            onClick={onClick}
+            className="bg-emerald-500 h-full hover:bg-emerald-600 text-white font-bold py-4 px-6 text-xl rounded"
+        >
+            {text}
+        </button>
+    );
+}
+
+export function DestructiveButton({ text, onClick }: ButtonProps) {
+    return (
+        <button
+            onClick={onClick}
+            className="bg-red-500 hover:bg-red-600 text-white font-bold py-4 px-8 text-xl rounded"
+        >
+            {text}
+        </button>
+    );
+}
